b773e81492504759e0181bdf397e39353a9693a5
Henrique <henrique.dias@gruponewway.com.br>
2023-04-22 15:14:04 -0300

    chore: validate csv

--- src/controller/csv.py
+++ src/controller/csv.py
@@ -9,13 +9,14 @@ from src.exceptions.custom_exception import CustomException
 
 csv_bp = Blueprint('csv_bp', __name__)
 
+ALLOWED_DELIMITERS = [",", ";"]
+
 
 @csv_bp.route('/csv', methods=['POST'])
 def post():
     try:
         csv_file = request.files['file']
-        csv_data = validate_csv_file(csv_file)
-        data = csv_array_to_dict(csv_data)
+        data = csv_array_to_dict(csv_file)
         collection_name = CollectionService.create_collection(data)
         response = {'id': collection_name}
         return jsonify(response), 201, {'Content-Type': 'application/json'}
@@ -27,8 +28,7 @@ def post():
 def put(collection_name: str):
     try:
         csv_file = request.files['file']
-        csv_data = validate_csv_file(csv_file)
-        data = csv_array_to_dict(csv_data)
+        data = csv_array_to_dict(csv_file)
         count = CollectionService.clear_and_insert_many(
             collection_name, data)
         response = {"id": collection_name, "count": count}
@@ -52,21 +52,27 @@ def validate_csv_file(csv_file):
     if not csv_file.filename.endswith('.csv'):
         raise CustomException("File must be a CSV", status_code=400)
 
-    csv_data = csv_file.read().decode('utf-8').splitlines()
     try:
-        allowed_delimiters = [",", ";"]
-        dialect = csv.Sniffer().sniff(csv_data[0], delimiters=allowed_delimiters)
-        if dialect.delimiter not in allowed_delimiters:
-            raise Exception("Invalid delimiter")
-        reader = csv.reader(csv_data, delimiter=dialect.delimiter)
-        for row in reader:
-            pass
-    except Exception as e:
+        csv_data = csv_file.read().decode('utf-8')
+        dialect = csv.Sniffer().sniff(
+            csv_data[:1024], delimiters=ALLOWED_DELIMITERS)
+        if dialect.delimiter not in ALLOWED_DELIMITERS:
+            raise ValueError("Invalid delimiter")
+        reader = csv.reader(csv_data.splitlines(), delimiter=dialect.delimiter)
+        headers = next(reader)
+        if not headers:
+            raise ValueError("CSV file must have headers")
+    except (ValueError, csv.Error) as e:
         raise CustomException(status_code=400, message=str(
             e), display_message="File must be a valid CSV")
-    csv_reader = csv.reader(csv_data)
-    headers = next(csv_reader, None)
-    if not headers:
-        raise CustomException("CSV file must have headers", status_code=400)
 
     return csv_data
+
+
+def csv_array_to_dict(csv_file):
+    contents = validate_csv_file(csv_file)
+    delimiter = ',' if ',' in contents[:100] else ';'
+    rows = csv.reader(contents.splitlines(), delimiter=delimiter)
+    headers = next(rows)
+    data = [dict(zip(headers, row)) for row in rows]
+    return data
